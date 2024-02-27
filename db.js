f568f1df30fdfd4e8ba084ad9293b811f1439894
Hydra75 <60604363+Hydra75@users.noreply.github.com>
2024-02-27 10:59:20 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  'process.env.DB_name', // Database name
+  'process.env.DB_user', // User
+  'process.env.DB_password', // Password
   {
     host: '', // Host
     dialect: 'postgres',
