2fe879ff36674f0ab533015338acf5e3f0f23a63
Hydra75 <60604363+Hydra75@users.noreply.github.com>
2024-02-27 11:09:06 +0100

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   'process.env.DB_user', // User
   'process.env.DB_password', // Password
   {
-    host: '', // Host
+    host: 'process.env.DB_host', // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
